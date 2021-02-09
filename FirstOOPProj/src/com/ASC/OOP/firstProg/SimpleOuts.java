4848d3111c49f2b7dd9cb2e6d39d12dfdb1cde5f
mabub03 <mabub03@gmail.com>
2021-02-09 10:40:41 -0500

    First Commit

---
+++ FirstOOPProj/src/com/ASC/OOP/firstProg/SimpleOuts.java
@@ -0,0 +1,13 @@
+package com.ASC.OOP.firstProg;
+
+public class SimpleOuts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int foo = 50;
+		
+		System.out.println("This is our first Java Program!");
+		System.out.println("output created with hotkeys");
+	}
+
+}
